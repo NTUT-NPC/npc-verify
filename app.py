e3e384a67086d1d95a46e704d8278902c70015c8
yaoandy107 <yaoandy107@gmail.com>
2018-09-23 18:14:12 +0800

    Add name and class to API

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import hashlib
 import sqlite3
 from flask_cors import CORS
 from  pymongo import MongoClient
+import json
 
 app = Flask(__name__)
 CORS(app)
-m = MongoClient('mongo', port=27017)
+m = MongoClient('localhost', port=27017)
 db = m.npc_db
 collection = db.record_activity
 
@@ -18,15 +19,16 @@ def recordActivity(json):
 # API: 驗證社員 id 是否在資料庫中
 @app.route('/v1/verify', methods=['POST'])
 def verify():
-    json = request.get_json(force=True)
-    uid = json['uid']
-    data = json['data']
+    reqjson = request.get_json(force=True)
+    uid = reqjson['uid']
+    data = reqjson['data']
     conn = sqlite3.connect('accounts.db')
     c = conn.cursor()
-    c.execute('SELECT uid FROM accounts where uid=?', (uid,))
-    if c.fetchone():
+    c.execute('SELECT * FROM accounts where uid=?', (uid,))
+    info = c.fetchone()
+    if info:
         recordActivity(data)
-        return Response(status=200)
+        return Response(json.dumps({'uid': info[2], 'name': info[2], 'class': info[3]}), mimetype='application/json', status=200)
     else:
         return Response(status=403)
 
@@ -43,14 +45,18 @@ def generate_uid(studentId):
 @app.route('/v1/member', methods=['POST'])
 def add_member():
     studentId = request.form['studentId']
+    studentClass = request.form['class']
+    name = request.form['name']
     uid = generate_uid(studentId)
     conn = sqlite3.connect('accounts.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS accounts(
         uid           TEXT     NOT NULL,
-        studentId     TEXT     NOT NULL);''')
-    c.execute('''INSERT INTO accounts (uid,studentId)
-        VALUES (?, ?)''', (uid, studentId))
+        studentId     TEXT     NOT NULL,
+        name          TEXT     NOT NULL,
+        class         TEXT     NOT NULL);''')
+    c.execute('''INSERT INTO accounts (uid,studentId, name, class)
+        VALUES (?, ?, ?, ?)''', (uid, studentId, name, studentClass))
     conn.commit()
     return Response(uid, status=200)
 
